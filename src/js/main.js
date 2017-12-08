86150a50b8024661e7cba0b65e7b80381d655988
Leonor Cuesta <leonorcu@ucm.es>
2017-12-08 21:09:18 +0100

    Creo que ahora al menos carga los dos archivos

--- src/js/main.js
+++ src/js/main.js
@@ -2,41 +2,33 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-
+var burbuja;
 
 function importarScript(nombre, callback) {
     var s = document.createElement("script");
     s.onload = callback;
     s.src = nombre;
-    document.querySelector("head").appendChild(s);
+   // document.querySelector("head").appendChild(s);
 }
 
 function scriptCargado(){
 	Console.log("33");
-	var burbuja = new Bubble(3, 400, 500, 'images/ball.png');
+	burbuja = new Bubble(3, 400, 500, 'images/ball.png');
+	burbuja.preload();
 
-	game.physics.enable(burbuja, Phaser.Physics.ARCADE);
-	burbuja.body.velocity.setTo(75, 200);
-	burbuja.body.collideWorldBounds = true;
-
-	burbuja.body.bounce.set(1);
-
-    burbuja.body.gravity.set(0, 201); 
 }
 
 
 
 function preload(){
-	game.load.baseURL = 'https://celiamartinezgraves.github.io/NullReference/src/';
 
-    game.load.crossOrigin = 'anonymous';
-    
 	var objects = importarScript("./objects.js", scriptCargado);
 }
 
 function create() {
-	
-
+	if (typeof burbuja !== 'undefined'){
+		burbuja.render(); 
+	}
 }
 
 function update(){
