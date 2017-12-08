8f569999df9dc47225c32d3b274ac847b4dca0e6
Leonor Cuesta <leonorcu@ucm.es>
2017-12-08 21:09:18 +0100

    Creo que ahora al menos carga los dos archivos

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,18 +1,28 @@
-//'use strict';
 
 var bublleSmall = 0.10;
 
 var Player = {
-	vidas = 3;
-
 };
 var Bubble = {
-	nivel;
-
+	
 	constructor (lvl, posX, posY, image){
-		nivel = lvl;
-		game.load.image('ballImage', image);
-		game.add.sprite(posY, posY, 'ballImage');
+		var nivel = lvl;
+		var imagen = image;
+	},
+
+	preload: function () {
+		game.load.image('ballImage', imagen);
 		this.scale.setTo(bublleSmall * nivel, bublleSmall*nivel);
+		game.physics.enable(this, Phaser.Physics.ARCADE);
+		this.body.velocity.setTo(75, 200);
+		this.body.collideWorldBounds = true;
+
+		this.body.bounce.set(1);
+
+    	this.body.gravity.set(0, 201);
+	},
+
+	render: function(){
+		game.add.sprite(posY, posY, 'ballImage');
 	}
 };
