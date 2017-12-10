ac5293b9963507450e2dac4845557e78fe41e8ac
Leonor Cuesta <leonorcu@ucm.es>
2017-12-10 11:54:22 +0100

    comentado objects.js

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,25 +1,41 @@
 
 class GameObject {
 
+// Se llama con new GameObject(x, y, img)
+// asigana los valores al gameObject para luego poder utilizarlos más adelante
 	constructor (posX, posY, image){
 		this.posX = posX;
 		this.posY = posY;
 		this.image = image;
-		console.log(image);
-		game.load.image('flyer', image);
-		console.log('carga imagen');
-
 	}
 
+// RESTO DE FUNCIONES
+// Aunque sean preload, create, etc, hay que hacer llamadas para que se activen
+// ya que esto es js no phaser (al ir por clases)
+
+// IMPORTANTE ASIGNAR ETIQUETA ANTES DE CREAR O CARGAR
+	setLabel(label){
+		this.label = label
+	}
+
+// carga la imagen con la etiqueta que sea
+// Si varios objetos utilizan la misma imagen, no es necesario cargarla en todos, 
+// con acargarla en el primer GO ya es suficiente porque todos tiran de la misma etiqueta en cache
 	preload(){
-		
+		game.load.image(this.label, this.image); //funciona
 	}
 
+// coloca en posX, posY
 	create(){
-		var ball = game.add.sprite(this.posX, this.posY, 'flyer');
-		console.log('colocado go');
+		this.obj = game.add.sprite(this.posX, this.posY, this.label); //funciona
+	}
+
+// cambia el tamaño a widthXheigth
+	resize(width, heigth){
+		this.obj.scale.setTo(width, heigth); //funciona
 	}
 }
+
 
 class Movable extends GameObject {
 	constructor(posX, posY, image, speedX, speedY){
