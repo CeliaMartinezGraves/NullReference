f9967425d8509865180cb021543ff3fc08205722
Leonor Cuesta <leonorcu@ucm.es>
2017-12-10 14:07:14 +0100

    plataformas con una pelota que rebota en Y con el suelo

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,38 @@
 //'use strict';
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-var go, go2;
+var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update: update, render: render });
+
+var go, go2, ball;
 
 function preload(){
-	go = new GameObject(200,200, 'images/ball.png');
-	go2 = new GameObject(300,300, 'images/ball.png');
-	go.setLabel('ball');
-	go2.setLabel('ball');
+
+	go = new GameObject(200,200, 'images/platform.png');
+	go2 = new GameObject(300,300, 'images/platform.png');
+	go.setLabel('platform');
+	go2.setLabel('platform');
 	go.preload();
+
+	ball = new Movable(300, 100, 'images/ball.png', 50, 50);
+	ball.setLabel('ball');
+	ball.preload();
 
 }
 
 function create() {
+
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.physics.arcade.gravity.y = 200;
+		
 	go.create();
 	go2.create();
+	ball.create();
+	
+
 	console.log('cargado go completo');
-	go.resize(0.15,0.15);
-	go2.resize(0.20, 0.20);
+	go.resize(1, 0.5);
+	go2.resize(1, 0.5);
+	ball.resize(0.2, 0.2);
+	
 }
 
 function update(){
