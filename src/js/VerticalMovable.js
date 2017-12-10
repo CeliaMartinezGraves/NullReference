160cd4e718a381f526dfee4871e730b7d5a7164f
Leonor Cuesta <leonorcu@ucm.es>
2017-12-10 17:30:53 +0100

    clases VerticalMovable y Bubble incompletos (solo movimiento)

---
+++ src/js/VerticalMovable.js
@@ -0,0 +1,5 @@
+class VerticalMovable extends collideWorld{
+	constructor(posX, posY, image, label, speedY){
+		super(posX, posY, image, label, 0, speedY);
+	}
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update: update, render: render });
 
-var go, go2, ball;
+var go, go2, ball, fall;
 
 function preload(){
 
@@ -11,20 +11,24 @@ function preload(){
 
 	go.preload();
 
-	ball = new Movable(300, 100, 'images/ball.png', 'ball', -50, 50);
+	ball = new Bubble(300, 100, 'images/ball.png', 'ball', -100, 150);
 	ball.preload();
+
+	fall = new VerticalMovable(500, 100, 'images/phaser.png', 'fall', 50);
+	fall.preload();
 
 }
 
 function create() {
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.physics.arcade.gravity.y = 200;
+	//game.physics.arcade.gravity.y = 200;
 
 	go.create();
 	go2.create();
 	ball.create();
-	
+	fall.create();
+	fall.resize(0.1,0.1);
 
 	console.log('cargado go completo');
 	go.resize(1, 0.5);
