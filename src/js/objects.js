3dc8022ae9a3c55111c54e94de455ff7d1da3cae
Leonor Cuesta <leonorcu@ucm.es>
2017-12-10 14:17:33 +0100

    las fisicas de phaser ya funcionan en los movable

--- src/js/objects.js
+++ src/js/objects.js
@@ -43,15 +43,19 @@ class Movable extends GameObject {
 		super(posX, posY, image);
 		this.speedX = speedX;
 		this.speedY = speedY;
-
-		
 	}
 
 	create(){
 		super.create();
 		game.physics.arcade.enable(this.obj);
-		this.obj.body.bounce.y = 0.95;
+
+		this.obj.body.velocity.setTo(this.speedX, this.speedY);
+
+		// ESTO NO VA EN ESTA CLASE; ES POR DEBUG
 		this.obj.body.collideWorldBounds = true;
+		this.obj.body.bounce.y = 1.001;
+		this.obj.body.bounce.x = 1.001;
+		
 	}
 	update(){
 
@@ -60,10 +64,9 @@ class Movable extends GameObject {
 }
 
 
+//this.obj.body.bounce.y = 1.001; para los rebotes en y
 
-
-
-
+//this.obj.body.collideWorldBounds = true; esto en todo menos animales
 
 
 
