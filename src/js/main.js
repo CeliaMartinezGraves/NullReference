f86e37fe53fe0aae2dfcf6b88552036bc07c8589
Leonor Cuesta <leonorcu@ucm.es>
2017-12-10 11:54:22 +0100

    comentado objects.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
 //'use strict';
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-var go;
+var go, go2;
 
 function preload(){
 	go = new GameObject(200,200, 'images/ball.png');
+	go2 = new GameObject(300,300, 'images/ball.png');
+	go.setLabel('ball');
+	go2.setLabel('ball');
 	go.preload();
 
 }
 
 function create() {
 	go.create();
+	go2.create();
 	console.log('cargado go completo');
+	go.resize(0.15,0.15);
+	go2.resize(0.20, 0.20);
 }
 
 function update(){
