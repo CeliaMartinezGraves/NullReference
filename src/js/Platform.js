53a0f4c7a59e76d9e43787e0b10c32c6f079c64c
L <leonorcu@ucm.es>
2018-01-25 01:36:28 +0100

    Niveles revisados para nuevo tam de platform

--- src/js/Platform.js
+++ src/js/Platform.js
@@ -1,10 +1,13 @@
 class Platform extends GameObject{
-	constructor(posX, posY, label){
+	constructor(posX, posY, label, rotar){
 		super(posX, posY, label);
 		this.body.immovable = true; // añade la propiedad inmóvil
 		this.addAnim('construccion',[1]);
 		this.addAnim('normal',[0]);
 		this.normal = true;
+		if(rotar === 1){
+			this.angle = 90;
+		}
 	}
 
 	platDeLaConstruccion(){
